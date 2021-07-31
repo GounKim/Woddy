50ac4fa9c8c1c4aee9c24ae34ccde1a8f62bd6cd
seonggg <seongeun7877@naver.com>
2021-07-31 12:57:51 +0900

    albumboard_listview

--- app/src/main/java/com/example/woddy/ActivityBase.java
+++ app/src/main/java/com/example/woddy/ActivityBase.java
@@ -25,90 +25,13 @@ public class ActivityBase extends AppCompatActivity {
     Toolbar mToolbar;
     TextView toolbarTitle;
 
-//    //bottomnavigation
-    FrameLayout main_layout;
-    BottomNavigationView bottomNavigationView;
 
-    private String TAG = "메인";
-
-    //프래그먼트 변수
-    Fragment fragment_home;
-    Fragment fragment_post;
-    Fragment fragment_chatting;
-    Fragment fragment_my;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
-        Log.i(TAG, "시작");
 
-        //프래그먼트 생성
-        fragment_home = new FragmentHome();
-        fragment_post = new FragmentPost();
-        fragment_chatting = new FragmentChatting();
-        fragment_my = new FragmentMy();
-
-        init();
-        SettingListener();
-
-        //처음화면
-        //getSupportFragmentManager().beginTransaction().add(R.id.main_layout, new FragmentHome()).commit();
-        bottomNavigationView.setSelectedItemId(R.id.menu_home);
-
-//        //리스너 등록
-//        bottomNavigationView.setOnItemSelectedListener(item -> {
-//            Log.i(TAG, "바텀 네비게이션 클릭");
-//
-//            switch(item.getItemId()){
-//                case R.id.menu_home:
-//                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,new FragmentHome()).commit();
-//                    break;
-//                case R.id.menu_post:
-//                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,new FragmentPost()).commit();
-//                    break;
-//                case R.id.menu_chatting:
-//                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,new FragmentChatting()).commit();
-//                    break;
-//                case R.id.menu_my_page:
-//                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,new FragmentMy()).commit();
-//                    break;
-//
-//            }
-//            return true;
-//        });
-    }
-
-    private void init() {
-        main_layout = findViewById(R.id.main_layout);
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-    }
-
-    private void SettingListener() {
-        //선택 리스너 등록
-        bottomNavigationView.setOnItemSelectedListener(new BottomNaviSelectedListener());
-    }
-
-    class BottomNaviSelectedListener implements NavigationBarView.OnItemSelectedListener{
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-                case R.id.menu_home:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,new FragmentHome()).commit();
-                    return true;
-                case R.id.menu_post:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,new FragmentPost()).commit();
-                    return true;
-                case R.id.menu_chatting:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,new FragmentChatting()).commit();
-                    return true;
-                case R.id.menu_my_page:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,new FragmentMy()).commit();
-                    return true;
-
-            }
-            return false;
-        }
     }
 
 

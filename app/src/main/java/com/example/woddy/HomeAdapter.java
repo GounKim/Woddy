9ba55e67227feedb219121dca14c432ceba6a8cb
GounKim <kgukgu2001@gmail.com>
2021-08-03 16:26:22 +0900

    HomeFragment setting

---
+++ app/src/main/java/com/example/woddy/HomeAdapter.java
@@ -0,0 +1,109 @@
+package com.example.woddy;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.woddy.Entity.ChattingInfo;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.Holder> {
+
+    private ArrayList<String> homeBoards;
+    private ArrayList<HomeBoardAdapter> pAdapter;
+
+    public HomeAdapter() {
+        this.pAdapter = new ArrayList<>();
+        this.homeBoards = new ArrayList<>();
+    }
+
+    public void addItem(String name, HomeBoardAdapter adapter) {
+        homeBoards.add(name);
+        pAdapter.add(adapter);
+//        notifyDataSetChanged();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        private final TextView boardName;
+        private final TextView itemMore;
+        private final ListView listView;
+
+        public Holder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            boardName = itemView.findViewById(R.id.home_board_name);
+            itemMore = itemView.findViewById(R.id.home_item_more);
+            listView = itemView.findViewById(R.id.home_board_listView);
+        }
+
+        public TextView getBoardName() {
+            return boardName;
+        }
+        public TextView getItemMore() {
+            return itemMore;
+        }
+        public ListView getListView() {
+            return listView;
+        }
+    }
+
+
+    @NonNull
+    @NotNull
+    @Override
+    public HomeAdapter.Holder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.home_item, parent, false);
+
+        Log.d(TAG, "in-----------");
+
+        return new HomeAdapter.Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull HomeAdapter.Holder holder, int position) {
+        holder.boardName.setText(homeBoards.get(position));
+        holder.listView.setAdapter(pAdapter.get(position));
+        setListViewHeight(holder.listView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pAdapter.size();
+    }
+
+    public static void setListViewHeight(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+
+        int height = 0;
+        int width = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(width, View.MeasureSpec.UNSPECIFIED);
+            height += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = height + (listView.getDividerHeight() * (listAdapter.getCount() -1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+}
--- app/src/main/java/com/example/woddy/HomeFragment.java
+++ app/src/main/java/com/example/woddy/HomeFragment.java
@@ -3,21 +3,31 @@ package com.example.woddy;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.woddy.DB.FirestoreManager;
 import com.example.woddy.DB.InitDBdata;
 
 import java.util.ArrayList;
 
+import static android.content.ContentValues.TAG;
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+
 public class HomeFragment extends Fragment {
-    TextView textView;
-    Button button;
+    RecyclerView recyclerView;
+    HomeBoardAdapter myAdapter;
+    HomeAdapter homeAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,24 +35,24 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        textView = view.findViewById(R.id.text);
-        button = view.findViewById(R.id.button2);
+        recyclerView = view.findViewById(R.id.home_recyclerView);
 
-//        try {
-//            InitDBdata init = new InitDBdata();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        myAdapter = new HomeBoardAdapter();
+        myAdapter.addItem(new HomePostings("writer1", "content1", "board1", 100, "18:30"));
+        myAdapter.addItem(new HomePostings("writer2", "content2", "board2", 123, "8:30", String.valueOf(R.drawable.sample_image2)));
+        myAdapter.addItem(new HomePostings("writer3", "content3", "board1", 10, "5:51"));
+        myAdapter.addItem(new HomePostings("writer4", "content4", "board3", 140, "13:13", String.valueOf(R.drawable.sample_image4)));
+        myAdapter.addItem(new HomePostings("writer5", "content5", "board2", 301, "20:02"));
+        homeAdapter = new HomeAdapter();
+        homeAdapter.addItem("알려드려요", myAdapter);
+//        HomeBoardAdapter a = new HomeBoardAdapter();
+//        a.addItem(new HomePostings("writer3", "content3", "board1", 10, "5:51"));
+//        a.addItem(new HomePostings("writer4", "content4", "board3", 140, "13:13", String.valueOf(R.drawable.sample_image4)));
+//        homeAdapter.addItem("현재 인기글", a);
+        homeAdapter.addItem("최신글", myAdapter);
 
-        textView.setText("");
-        FirestoreManager firestoreManager = new FirestoreManager();
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textView.setText("");
-                firestoreManager.getChatRoomList("user1");
-            }
-        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(), recyclerView.VERTICAL, false)); // 상하 스크롤
+        recyclerView.setAdapter(homeAdapter);
 
         return view;
     }

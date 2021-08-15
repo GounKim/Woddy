620fe33c4593fa5f94865b051da2aed37250b161
seonggg <seongeun7877@naver.com>
2021-08-15 10:16:28 +0900

    pull

---
+++ app/src/main/java/com/example/woddy/Notice/Fragment/HabitFragment.java
@@ -0,0 +1,27 @@
+package com.example.woddy.Notice.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.woddy.R;
+
+public class HabitFragment extends Fragment {
+
+
+    public HabitFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.notice_page_habit, container, false);
+    }
+}

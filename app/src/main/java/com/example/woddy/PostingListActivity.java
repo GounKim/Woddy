20a61c939ab27c77d9dfce421ed3c243e97c72ef
GounKim <kgukgu2001@gmail.com>
2021-08-18 18:54:06 +0900

    좋아요 완료

--- app/src/main/java/com/example/woddy/PostingListActivity.java
+++ app/src/main/java/com/example/woddy/PostingListActivity.java
@@ -9,19 +9,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.woddy.DB.FirestoreManager;
 import com.example.woddy.DB.SQLiteManager;
 import com.example.woddy.Entity.Posting;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -67,6 +72,7 @@ public class PostingListActivity extends BaseActivity {
                 break;
             case 2:
                 setMyTitle("내가 좋아요 누른 글");
+                myLikedPost();
                 break;
             case 3:
                 setMyTitle("내가 스크랩한 글");
@@ -106,31 +112,26 @@ public class PostingListActivity extends BaseActivity {
     }
 
     private void myLikedPost() {
-        manager.getPostWithWriter(user).get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            QuerySnapshot result = task.getResult();
-                            if (!result.isEmpty()) {
-                                ArrayList<Posting> postings = new ArrayList<>();
-                                ArrayList<String> path = new ArrayList<>();
-                                for (QueryDocumentSnapshot document : task.getResult()) {
-                                    postings.add(document.toObject(Posting.class));
-                                    path.add(document.getReference().getPath());
-                                }
-                                adapter.setItem(postings, path);
+        ArrayList<String> likePostPath = sqlManager.getLiked();
+        adapter.setPathItem(likePostPath);
 
+        for (String path : likePostPath) {
+            manager.getdocRefWithPath(path).get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.getResult() != null) {
+                                DocumentSnapshot documentSnapshot = task.getResult();
+                                Posting posting = documentSnapshot.toObject(Posting.class);
+                                adapter.addPosting(posting);
+                                Log.d(TAG, "posting: " + posting.getWriter());
                             } else {
-                                textView.setText("내가 작성한 글이 없습니다.");
-                                textView.setVisibility(View.VISIBLE);
-                                Log.d(TAG, "nothing found.", task.getException());
+                                adapter.addPosting(new Posting());
+                                Log.d(TAG, "finding my posting task failed. error: " , task.getException());
                             }
-                        } else {
-                            Log.d(TAG, "finding my posting task failed. error: " , task.getException());
                         }
-                    }
-                });
+                    });
+        }
     }
 
     private void myScrappedPost() {

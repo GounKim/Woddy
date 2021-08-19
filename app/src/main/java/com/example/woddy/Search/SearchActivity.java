2001e84714dda345446d52555dfd855d956ee31d
SeongHye <87599027+seongzzang@users.noreply.github.com>
2021-08-19 19:04:52 +0900

    upload search

--- app/src/main/java/com/example/woddy/Search/SearchActivity.java
+++ app/src/main/java/com/example/woddy/Search/SearchActivity.java
@@ -20,15 +20,16 @@ import com.google.android.material.chip.ChipGroup;
 
 public class SearchActivity extends AppCompatActivity {
 
-    ChipGroup chipGroup;
+    ChipGroup chipGroup_info, chipGroup_board;
     Chip friend, help, mate, share, home, buy, freeShare, free, diy, interior, townInfo, club, meeting;
 
     ImageButton searchBtn,beforeBtn;
     EditText searchText;
     RecyclerView recyclerView;
 
-    private String board_name = "전체";
-    private String tagName = "전체";
+    private String board_name = "정보";
+    private String tagName = "생활지원";
+    private String boardType = "info";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,25 +47,8 @@ public class SearchActivity extends AppCompatActivity {
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
-        chipGroup = (ChipGroup) findViewById(R.id.filterChipGroup);
-
-        friend = (Chip) findViewById(R.id.chipFriend);
-        help = (Chip) findViewById(R.id.chipHelp);
-        mate = (Chip) findViewById(R.id.chipMate);
-
-        home = (Chip) findViewById(R.id.chipHome);
-        share = (Chip) findViewById(R.id.chipShare);
-        buy = (Chip) findViewById(R.id.chipBuy);
-        freeShare = (Chip) findViewById(R.id.chipFreeShare);
-
-        chipGroup = (ChipGroup) findViewById(R.id.filterChipGroup);
-        meeting = (Chip) findViewById(R.id.chipMeeting);
-        club = (Chip) findViewById(R.id.chipClub);
-
-        free = (Chip) findViewById(R.id.chipFreeTalk);
-        diy = (Chip) findViewById(R.id.chipDIY);
-        interior = (Chip) findViewById(R.id.chipInterior);
-        townInfo = (Chip) findViewById(R.id.chipTownInfo);
+        chipGroup_info = (ChipGroup) findViewById(R.id.filterChipGroup_info);
+        chipGroup_board = (ChipGroup) findViewById(R.id.filterChipGroup_board);
 
         beforeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,78 +57,117 @@ public class SearchActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        chipGroup_info.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(ChipGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.chipLifeSupport:
+                        board_name = "소통";
+                        tagName = "친구찾기";
+                        boardType = "info";
+                        break;
+                    case R.id.chipEducation:
+                        board_name = "소통";
+                        tagName = "도움요청";
+                        boardType = "info";
+                        break;
 
-        chipGroup.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
+                    case R.id.chipEvent:
+                        board_name = "소통";
+                        tagName = "퇴근메이트";
+                        boardType = "info";
+                        break;
+                }
+            }
+        });
+
+        chipGroup_board.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(ChipGroup group, int checkedId) {
                 switch (checkedId) {
+
+                    /*
                     case R.id.chipAll:
                         board_name = "전체";
                         tagName = "전체";
                         break;
+                     */
                     case R.id.chipFriend:
                         board_name = "소통";
                         tagName = "친구찾기";
+                        boardType = "normal";
                         break;
 
                     case R.id.chipHelp:
                         board_name = "소통";
                         tagName = "도움요청";
+                        boardType = "normal";
                         break;
 
                     case R.id.chipMate:
                         board_name = "소통";
                         tagName = "퇴근메이트";
+                        boardType = "normal";
                         break;
 
                     case R.id.chipShare:
                         board_name = "쉐어";
                         tagName = "물품공유";
+                        boardType = "normal";
                         break;
 
                     case R.id.chipHome:
                         board_name = "쉐어";
                         tagName = "홈";
+                        boardType = "album";
                         break;
 
                     case R.id.chipBuy:
                         board_name = "쉐어";
                         tagName = "공동구매";
+                        boardType = "album";
                         break;
 
                     case R.id.chipFreeShare:
                         board_name = "쉐어";
                         tagName = "무료나눔";
+                        boardType = "album";
                         break;
 
                     case R.id.chipClub:
                         board_name = "취미";
                         tagName = "동호회";
+                        boardType = "normal";
                         break;
 
                     case R.id.chipMeeting:
                         board_name = "취미";
                         tagName = "번개모임";
+                        boardType = "normal";
                         break;
 
                     case R.id.chipFreeTalk:
                         board_name = "자유";
                         tagName = "자유";
+                        boardType = "normal";
                         break;
 
                     case R.id.chipDIY:
                         board_name = "자유";
                         tagName = "DIY";
+                        boardType = "album";
                         break;
 
                     case R.id.chipInterior:
                         board_name = "자유";
                         tagName = "인테리어";
+                        boardType = "album";
                         break;
 
                     case R.id.chipTownInfo:
                         board_name = "자유";
                         tagName = "동네정보";
+                        boardType = "normal";
                         break;
                     default:
                         Toast.makeText(SearchActivity.this, "태그를 선택해주세요", Toast.LENGTH_SHORT).show();
@@ -156,7 +179,14 @@ public class SearchActivity extends AppCompatActivity {
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new SearchData(SearchActivity.this).getItems(recyclerView, board_name, tagName, searchText.getText().toString());
+                System.out.println(boardType);
+                if(boardType == "normal") {
+                    new SearchData().getItems(recyclerView, board_name, tagName, searchText.getText().toString());
+                } else if(boardType == "album"){
+                    new SearchAlbumData().getItems(recyclerView, board_name, tagName, searchText.getText().toString());
+                } else {
+                    new SearchInfoData().getItems(recyclerView, board_name, tagName, searchText.getText().toString());
+                }
             }
         });
     }
